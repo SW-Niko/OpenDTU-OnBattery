186c2a5cbc0c403ae7972c07bee23fb2472dd24d
SW-Nico <stbauer.privat@web.de>
2026-04-01 23:38:56 +0200

    Runtime2: improved integration
    - Better central place to call Runtime.writeAll() on restart or update
    - interface integration into main()

--- src/main.cpp
+++ src/main.cpp
@@ -148,6 +148,7 @@ void setup()
     RestartHelper.init(scheduler);
 
     // OpenDTU-OnBattery-specific initializations go between here...
+    Runtime.init(scheduler); // Runtime must be initialized before the components below can register providers to it
     SolarCharger.init(scheduler);
     PowerMeter.init(scheduler);
     PowerLimiter.init(scheduler);
@@ -156,10 +157,7 @@ void setup()
     BatteryGuard.init(scheduler);
     // ... and here (before RuntimeData)
 
-    // Must be done after all other components have been initialized
-    RuntimeData.init(scheduler);
-    RuntimeData.read();
-
+    Runtime.read(); // Read runtime values from all components that are registered to read on startup
 
     ESP_LOGI(TAG, "Startup complete");
 }
